a48052d85d14c199ad443795e5405d8af37201ee
Josh Beard <josh@jlbeard.com>
2020-09-23 22:29:23 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestEnetClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
