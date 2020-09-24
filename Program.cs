5d74816f4150341ecef043a73418c2b8e2e4f372
Josh Beard <josh@jlbeard.com>
2020-09-23 22:45:35 -0400

    Adding enet logic

--- Program.cs
+++ Program.cs
@@ -1,12 +1,77 @@
 ﻿using System;
+using ENet;
 
 namespace TestEnetClient
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private const string ServerHostName = "localhost";
+        private const int ServerPort = 8888;
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var client = new Host()) 
+            {
+                Console.WriteLine("Starting server...");
+
+                var address = new Address
+                {
+                    Port = ServerPort
+                };
+
+                address.SetHost(ServerHostName);
+
+                client.Create();
+
+                Console.WriteLine($"Connecting to host {ServerHostName} on {address.Port}");
+
+                var peer = client.Connect(address);
+
+                Event netEvent;
+
+                while (!Console.KeyAvailable) 
+                {
+                    var polled = false;
+
+                    while (!polled) 
+                    {
+                        if (client.CheckEvents(out netEvent) <= 0) 
+                        {
+                            if (client.Service(15, out netEvent) <= 0)
+                            {
+                                break;
+                            }
+
+                            polled = true;
+                        }
+
+                        switch (netEvent.Type) 
+                        {
+                            case EventType.None:
+                                break;
+
+                            case EventType.Connect:
+                                Console.WriteLine("Client connected to server");
+                                break;
+
+                            case EventType.Disconnect:
+                                Console.WriteLine("Client disconnected from server");
+                                break;
+
+                            case EventType.Timeout:
+                                Console.WriteLine("Client connection timeout");
+                                break;
+
+                            case EventType.Receive:
+                                Console.WriteLine("Packet received from server - Channel ID: " + netEvent.ChannelID + ", Data length: " + netEvent.Packet.Length);
+                                netEvent.Packet.Dispose();
+                                break;
+                        }
+                    }
+                }
+
+                client.Flush();
+            }
         }
     }
 }
